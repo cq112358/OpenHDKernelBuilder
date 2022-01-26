add6fc3d0e9e98f6de84ce787eaedc24522eb59f
cq112358 <98128829+cq112358@users.noreply.github.com>
2022-01-26 00:36:11 -0500

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -19,7 +19,7 @@ function init() {
 function package() {
     PACKAGE_NAME=openhd-linux-${PLATFORM}
 
-    VERSION=$(git describe --tags | sed 's/\(.*\)-.*/\1/')
+    VERSION=20220127.1
 
     rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
 
